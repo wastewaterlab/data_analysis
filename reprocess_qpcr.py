4ba8dfbf5a92973e1be71d482fb29b1e7758d17f
Lauren Kennedy <kennedylaurenc@berkeley.edu>
2020-10-05 13:24:52 -0700

    fixed typo

--- reprocess_qpcr.py
+++ reprocess_qpcr.py
@@ -199,7 +199,7 @@ def get_pass_grubbs_test(plate_df, groupby_list):
     d = df.copy() # avoid set with copy warning
 
     # make new column 'grubbs_test' that includes the results of the test
-    if (len(d.Cq)<3)): #cannot evaluate for fewer than 3 values
+    if (len(d.Cq)<3): #cannot evaluate for fewer than 3 values
         d.loc[:, 'grubbs_test'] = False
         plate_df_with_grubbs_test.append(d)
     else:
