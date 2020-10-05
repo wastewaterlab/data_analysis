42c3771d21bd91a71d844b720d7ab91923e41053
Lauren Kennedy <kennedylaurenc@berkeley.edu>
2020-10-05 09:36:17 -0700

    replaced mean and std with gmean and gstd for Quantity and Cq

--- reprocess_qpcr.py
+++ reprocess_qpcr.py
@@ -4,8 +4,10 @@ from sklearn.linear_model import LinearRegression
 import sys
 import math
 from pandas.api.types import CategoricalDtype
-from scipy import stats
+from scipy import stats as sci
 from scipy.stats import linregress
+from scipy.stats.mstats import gmean
+from scipy.stats import gstd
 from sklearn.utils import resample
 import pdb
 from sklearn.metrics import r2_score
@@ -256,15 +258,15 @@ def combine_triplicates(plate_df_in, checks_include):
 
     plate_df_avg = plate_df.groupby(groupby_list).agg(
                                                template_volume=('template_volume','max'),
-                                               Q_init_mean=('Quantity', 'mean'),
-                                               Q_init_std=('Quantity', 'std'),
+                                               Q_init_mean=('Quantity', lambda x: sci.gmean(x,axis=0)),
+                                               Q_init_std=('Quantity', lambda x: np.nan if ( len(x) <2 ) else (sci.gstd(x,axis=0))),
                                                Q_init_CoV=('Quantity',lambda x: np.std(x) / np.mean(x)),
                                                Cq_init_mean=('Cq', 'mean'),
                                                Cq_init_std=('Cq', 'std'),
                                                Cq_init_min=('Cq', 'min'),
                                                replicate_init_count=('Cq','count'),
-                                               Cq_mean=('Cq_copy', 'mean'),
-                                               Cq_std=('Cq_copy', 'std'),
+                                               Cq_mean=('Cq', lambda x: sci.gmean(x,axis=0)),
+                                               Cq_std=('Cq', lambda x: np.nan if ( len(x) <2 ) else (sci.gstd(x,axis=0))),
                                                replicate_count=('Cq_copy', 'count'),
                                                is_undetermined_count=('is_undetermined', 'sum')
                                                )
