28fb465e3b9cc03ce7e3c3e8e284910dc24772f2
Lauren Kennedy <kennedylaurenc@berkeley.edu>
2020-10-06 14:57:29 -0700

    added Cq_of_lowest_std_quantity to standard df

--- reprocess_qpcr.py
+++ reprocess_qpcr.py
@@ -472,6 +472,8 @@ def process_qpcr_raw(qpcr_raw, checks_include):
                                                         'efficiency',
                                                         'ntc_result'])
     qpcr_processed = pd.concat(qpcr_processed)
+    qpcr_m=qpcr_processed[["plate_id","Cq_of_lowest_std_quantity"]].copy().drop_duplicates(keep='first')
+    std_curve_df=std_curve_df.merge(qpcr_m, how='left')
     qpcr_processed = qpcr_processed.merge(std_curve_df, how='left', on=['plate_id', 'Target'])
     qpcr_processed= determine_samples_BLoQ(qpcr_processed, 40)
     return(qpcr_processed, std_curve_df, raw_outliers_flagged_df)
