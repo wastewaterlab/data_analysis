7f8565626e323c40555aaf1d1a67115d5053c146
Lauren Kennedy <kennedylaurenc@berkeley.edu>
2020-10-06 11:22:14 -0700

    attempting to fix mean of an empty slice error

--- reprocess_qpcr.py
+++ reprocess_qpcr.py
@@ -303,15 +303,15 @@ def combine_triplicates(plate_df_in, checks_include):
 
     plate_df_avg = plate_df.groupby(groupby_list).agg(
                                                template_volume=('template_volume','max'),
-                                               Q_init_mean=('Quantity', lambda x: sci.gmean(x.dropna(),axis=0)),
-                                               Q_init_std=('Quantity', lambda x: np.nan if ( len(x.dropna()) <2 ) else (sci.gstd(x.dropna(),axis=0))),
+                                               Q_init_mean=('Quantity', lambda x: np.nan if all(np.isnan(x)) else sci.gmean(x.dropna(),axis=0)),
+                                               Q_init_std=('Quantity', lambda x: np.nan if ( (len(x.dropna()) <2 )| all(np.isnan(x)) ) else (sci.gstd(x.dropna(),axis=0))),
                                                Q_init_CoV=('Quantity',lambda x: np.std(x.dropna()) / np.mean(x.dropna())),
                                                Cq_init_mean=('Cq', 'mean'),
                                                Cq_init_std=('Cq', 'std'),
                                                Cq_init_min=('Cq', 'min'),
                                                replicate_init_count=('Cq','count'),
-                                               Cq_mean=('Cq', lambda x: sci.gmean(x.dropna(),axis=0)),
-                                               Cq_std=('Cq', lambda x: np.nan if ( len(x.dropna()) <2 ) else (sci.gstd(x.dropna(),axis=0))),
+                                               Cq_mean=('Cq', lambda x:  np.nan if all(np.isnan(x)) else sci.gmean(x.dropna(),axis=0)),
+                                               Cq_std=('Cq', lambda x: np.nan if ((len(x.dropna()) <2 )| all(np.isnan(x)) ) else (sci.gstd(x.dropna(),axis=0))),
                                                replicate_count=('Cq_copy', 'count'),
                                                is_undetermined_count=('is_undetermined', 'sum')
                                                )
