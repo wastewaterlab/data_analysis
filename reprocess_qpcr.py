ef08c604ae141290ef858f2a74e4977bd1a44ce9
Lauren Kennedy <kennedylaurenc@berkeley.edu>
2020-10-09 17:50:23 -0700

    same as previous

--- reprocess_qpcr.py
+++ reprocess_qpcr.py
@@ -480,8 +480,8 @@ def determine_samples_BLoQ(qpcr_p, max_cycles, out_fin, include_LoD=False):
         qpcr_p["blod"]= np.nan
         targs=qpcr_p.Target.unique()
         for target in targs:
-            C_value=list(out_fin.loc[(out_fin.Target==target),"LoD_Cq"])
-            Q_value=list(out_fin.loc[(out_fin.Target==target),"LoD_Quantity"])
+            C_value=out_fin.loc[(out_fin.Target==target),"LoD_Cq"]
+            Q_value=out_fin.loc[(out_fin.Target==target),"LoD_Quantity"]
             print(C_value)
             if np.isnan(C_value):
                 qpcr_p.loc[(qpcr_p.Target==target)&(qpcr_p.Cq_mean > C_value),"bloq"]= np.nan
