32833ea146c9fe2c69b61e83169f0495804bbac5
Rose Kantor <rsgkantor@gmail.com>
2020-10-20 15:11:34 -0700

    added get_extraction_control() for PBS batch controls

--- qa_qc.py
+++ qa_qc.py
@@ -2,6 +2,38 @@
 import pandas as pd
 import numpy  as np
 
+def get_pbs_control(qpcr_averaged):
+    '''
+    make column for PBS controls assessment
+    Params
+        qpcr_averaged df, with columns:
+        'plate_id', 'batch', 'Target' and 'Cq_init_min'
+    Returns
+        qpcr_averaged df without rows for PBS controls
+        with new column 'PBS_result' for the PBS extraction batch control
+    '''
+
+    # filter for just PBS controls and specific columns
+    pbs = qpcr_averaged[qpcr_averaged.interceptor == 'PBS'].copy()
+    pbs.Cq_init_min = pd.to_numeric(PBS.Cq_init_min)
+
+    # make PBS_result column "negative" if Cq was NaN/nondetect
+    # or equal to Cq_min of PBS if detected
+    pbs['PBS_result'] = np.nan
+    pbs.loc[np.isnan(pbs.Cq_init_min), 'PBS_result'] = 'negative'
+    pbs.loc[~np.isnan(pbs.Cq_init_min), 'PBS_result'] = pbs.Cq_init_min
+
+    pbs = pbs[['plate_id', 'batch', 'Target', 'PBS_result']]
+
+    # filter out pbs controls from main dataset then merge pbs df
+    qpcr_averaged = qpcr_averaged[qpcr_averaged.interceptor!='PBS'].copy()
+    qpcr_averaged = qpcr_averaged.merge(pbs, how = 'left', on = ['plate_id', 'batch', 'Target'])
+
+    # make sure date_sampling is still datetime (sometime gets messed up with merges)
+    qpcr_averaged.date_sampling = pd.to_datetime(qpcr_averaged.date_sampling)
+
+    return(qpcr_averaged)
+
 def quality_score(p, dic_name, df):
    '''
    for a processed data frame, will give quality score info
