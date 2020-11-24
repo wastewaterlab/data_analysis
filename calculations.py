e9c9b971462e93b5c2e04fe6405f0f53920a666f
Rose Kantor <rsgkantor@gmail.com>
2020-11-24 12:20:29 -0800

    updated process_xeno_inhibition()

--- calculations.py
+++ calculations.py
@@ -110,74 +110,39 @@ def normalize_to_18S(qpcr_data, replace_bloq= False):
 
     return qpcr_m
 
-def xeno_inhibition_test(qpcr_data,qpcr_normd, x=1):
-  '''
-        Calculates the difference in Ct compared to the NTC for xeno inhibition test, outputs a list of inhibited samples
+def process_xeno_inhibition(qpcr_processed_dilutions_xeno, plate_target_info, plate_id, dCt_cutoff=1):
+    '''
+    takes single plate with Target == Xeno
+    finds NTC for that plate in the plate_target_info table, calculates gmean of Cq for Xeno
+    determines if samples were inhibited relative to NTC, based on the dCt_cutoff
 
-          Params
-            optional x: the dCt defined as inhibited
-            qpcr_data (main dfm)
-            qpcr_data_xeno-- dataframe with qpcr technical triplicates averaged. Requires the columns
-                    Target (includes xeno)
-                    plate_id
-                    Well
-                    Quantity_mean
-                    Sample
-                    Task
-          Returns
-          qpcr_data with is_inhibited column
-          xeno_fin_all -- calculates the difference in Ct values of the negative control (spiked with xeno) to the sample spiked with xeno, adds column for inhibited (Yes or No)
-          ntc_col -- all of the negative control values for xeno
-  '''
+    Params
+    qpcr_processed_dilutions_xeno: processed qPCR data still containing dilutions as separate rows, filtered by Target == 'Xeno'
+    plate_target_info: table containing info about plates, including NTCs and their Cqs
+    plate_id: the plate_id (coming from the groupby that feeds plates into this function)
+    dCt_cutoff: difference between Cq of a sample and Cq of the NTC that defines whether the sample was inhibited, 1 is rule of thumb
 
-  #Find targets other than xeno for each well+plate combination
-  p_w_targets=qpcr_data[qpcr_data.Target!='Xeno'].copy()
-  p_w_targets['p_id']=p_w_targets.plate_id.astype('str').str.cat(p_w_targets.Well.astype('str'), sep ="_")
-  p_w_targets=p_w_targets.groupby('p_id')['Target'].apply(lambda targs: ','.join(targs)).reset_index()
-  p_w_targets.columns=['p_id','additional_target']
+    Returns
+    qpcr_processed_dilutions_inhibition: dataframe with columns ['Sample', 'dilution', 'xeno_dCt', 'is_inhibited', 'plate_id']
+        to be merged with qpcr_processed_dilutions, indicating inhibition
+    '''
 
-  #subset out xeno samples, merge with previous, use to calculate mean and std
-  target=qpcr_data[(qpcr_data.Target=='Xeno')].copy() #includes NTC & stds
-  target['p_id']=qpcr_data.plate_id.astype('str').str.cat(qpcr_data.Well, sep ="_")
-  target=target.merge(p_w_targets, how='left', on='p_id')
-  if target.additional_target.astype('str').str.contains(',').any():
-      print(target.additional_target.unique())
-      raise ValueError('Error: update function, more than 2 multiplexed targets or one of the two multiplexed targets is not xeno')
+    # get the geometric mean of the Cqs for the NTCs on the plate
 
-  target_s=target.groupby(["Sample","sample_full",'additional_target','plate_id','Task']).agg(Ct_vet_mean=('Cq', lambda x:  np.nan if all(np.isnan(x)) else sci.gmean(x.dropna(),axis=0)),
-                                                                    Quantity_std_crv=('Quantity','max'), #just for standards
-                                                                    Ct_vet_std=('Cq', lambda x: np.nan if ((len(x.dropna()) <2 )| all(np.isnan(x)) ) else (sci.gstd(x.dropna(),axis=0))),
-                                                                    Ct_vet_count=('Cq','count')).reset_index()
-  target=target_s[(target_s.Task!='Standard')].copy() #remove standards
+    xeno_ntc_Cq_mean = np.nan
+    xeno_ntc = plate_target_info[(plate_target_info.plate_id == plate_id) & (plate_target_info.Target == 'Xeno')]
+    if (len(xeno_ntc) > 0) and (~xeno_ntc.ntc_Cq.isna().all()): # skip if there is no NTC with Xeno
+        xeno_ntc_Cq_list = xeno_ntc.ntc_Cq.values[0]
+        xeno_ntc_Cq_mean = sci.gmean(xeno_ntc_Cq_list)
 
-  #subset and recombine to get NTC as a col
-  ntc_col_c=target[target.Task=='Negative Control'].copy()
-  ntc_col=ntc_col_c[["plate_id",'additional_target','Ct_vet_mean']].copy()
-  ntc_col.columns=["plate_id",'additional_target','Ct_control_mean']
+    qpcr_processed_dilutions_xeno['xeno_dCt'] = qpcr_processed_dilutions_xeno.Cq_mean - xeno_ntc_Cq_mean
+    qpcr_processed_dilutions_xeno['is_inhibited'] = None
+    qpcr_processed_dilutions_xeno.loc[qpcr_processed_dilutions_xeno.xeno_dCt >= dCt_cutoff, 'is_inhibited'] = True
+    qpcr_processed_dilutions_xeno.loc[qpcr_processed_dilutions_xeno.xeno_dCt < dCt_cutoff, 'is_inhibited'] = False
 
-  ntc_col_c=ntc_col_c[["plate_id",'Task','Quantity_std_crv','additional_target','Ct_vet_mean']].copy()
-  ntc_col_c.columns=["plate_id",'Task','Quantity_std_crv','additional_target','Ct_control_mean']
+    qpcr_processed_dilutions_inhibition = qpcr_processed_dilutions_xeno[['Sample', 'dilution', 'xeno_dCt', 'is_inhibited', 'plate_id']]
 
-  std_col=target_s[target_s.Task=='Standard'].copy()
-  std_col=std_col[["plate_id", 'Task','Quantity_std_crv','additional_target','Ct_vet_mean']].copy()
-  std_col.columns=["plate_id",'Task','Quantity_std_crv','additional_target','Ct_control_mean']
-
-  xeno_fin_all=target[target.Task=='Unknown'].copy()
-  xeno_fin_all=xeno_fin_all.merge(ntc_col, how='left')
-  xeno_fin_all["dCt"]= (xeno_fin_all["Ct_vet_mean"]- xeno_fin_all["Ct_control_mean"])
-  xeno_fin_all["inhibited"]='No'
-  xeno_fin_all.loc[(xeno_fin_all.dCt>x),"inhibited"]="Yes"
-
-  ntc_std_control= ntc_col_c.append(std_col)
-
-  inhibited=xeno_fin_all[xeno_fin_all.dCt>1].Sample.unique()
-  not_inhibited=xeno_fin_all[xeno_fin_all.dCt<=1].Sample.unique()
-
-  qpcr_normd["is_inhibited"]='unknown'
-  qpcr_normd.loc[qpcr_normd.Sample.isin(inhibited),"is_inhibited"]= True
-  qpcr_normd.loc[qpcr_normd.Sample.isin(not_inhibited),"is_inhibited"]= False
-
-  return qpcr_normd, xeno_fin_all, ntc_std_control
+    return(qpcr_processed_dilutions_inhibition)
 
 def get_GFP_recovery(qpcr_averaged):
     '''
