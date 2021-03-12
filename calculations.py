42af8c1eed7bb63563e366e8d30018ba587af683
Rose Kantor <rsgkantor@gmail.com>
2021-03-11 20:41:01 -0800

    minor update: remove Task column from normalize_to_pmmov(), delete normalize_to_18S

--- calculations.py
+++ calculations.py
@@ -62,49 +62,12 @@ def normalize_to_pmmov(qpcr_data, replace_bloq= False):
         qpcr_data.loc[qpcr_data.bloq==True, "Quantity_mean"]= qpcr_data.lowest_std_quantity
 
     pmmov=qpcr_data[qpcr_data.Target=='PMMoV']
-    pmmov=pmmov[['Quantity_mean','Sample','Task']]
-    pmmov.columns=['pmmov_mean',  "Sample", "Task"]
-    qpcr_m=qpcr_data.merge(pmmov, how='left', on=["Sample", "Task"])
+    pmmov=pmmov[['Quantity_mean','Sample']]
+    pmmov.columns=['pmmov_mean',  "Sample"]
+    qpcr_m=qpcr_data.merge(pmmov, how='left', on=["Sample"])
     qpcr_m["mean_normalized_to_pmmov"] = qpcr_m['Quantity_mean']/qpcr_m['pmmov_mean']
     qpcr_m["log10mean_normalized_to_log10pmmov"] = np.log10(qpcr_m['Quantity_mean'])/np.log10(qpcr_m['pmmov_mean'])
     qpcr_m['log10_mean_normalized_to_pmmov']=np.log10(qpcr_m['mean_normalized_to_pmmov'])
-
-    return qpcr_m
-
-def normalize_to_18S(qpcr_data, replace_bloq= False):
-
-    '''
-        calculates a normalized mean to 18S when applicable and returns dataframe with new columns
-
-          Params
-        replace_bloq should the function replace any sample that is below the limit of quantification with the loq
-        values the values to replace the bloq values with (should be lower than the loq)
-            qpcr_data-- dataframe with qpcr technical triplicates averaged. Requires the columns
-                    Target
-                    Quantity_mean
-                    Sample
-                    Task
-            if replace_bloq is true
-                bloq
-                lowest_std_quantity
-          Returns
-          qpcr_m: same data, with additional columns
-                mean_normalized_to_18S: takes every column and divides by 18S that is associated with that sample name (so where target == 18S it will be 1)
-                log10mean_normalized_to_log1018S: takes the log10 of N1 and the log 10 of 18S then normalizes
-                log10_mean_normalized_to_18S: takes the log10 of mean_normalized_to_18S
-    '''
-
-    if replace_bloq:
-        qpcr_data.loc[qpcr_data.bloq==True, "Quantity_mean"]= qpcr_data.lowest_std_quantity
-
-    n_18S=qpcr_data[qpcr_data.Target=='18S']
-    n_18S=n_18S[['Quantity_mean','Sample','Task']]
-    n_18S.columns=['18S_mean',  "Sample", "Task"]
-    qpcr_m=qpcr_data.merge(n_18S, how='left', on=["Sample", "Task"])
-    qpcr_m["mean_normalized_to_18S"] = qpcr_m['Quantity_mean']/qpcr_m['18S_mean']
-    qpcr_m["log10mean_normalized_to_log1018S"] = np.log10(qpcr_m['Quantity_mean'])/np.log10(qpcr_m['18S_mean'])
-    qpcr_m['log10_mean_normalized_to_18S']=np.log10(qpcr_m['mean_normalized_to_18S'])
-
 
     return qpcr_m
 
