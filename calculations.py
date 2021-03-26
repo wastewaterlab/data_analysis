97d52611c008bdaec60195d00251ad90482084b7
Rose Kantor <rsgkantor@gmail.com>
2021-03-26 16:40:17 -0700

    remove normalize_to_pmmov() dead function

--- calculations.py
+++ calculations.py
@@ -39,42 +39,6 @@ def calculate_gc_per_ml(qpcr_data):
     return(qpcr_data['gc_per_mL'])
 
 
-def normalize_to_pmmov(qpcr_data, replace_bloq= False):
-
-    '''
-    calculates a normalized mean to pmmov when applicable and returns dataframe with new columns
-
-      Params
-        replace_bloq should the function replace any sample that is below the limit of quantification with the loq
-        values the values to replace the bloq values with (should be lower than the loq)
-        qpcr_data-- dataframe with qpcr technical triplicates averaged. Requires the columns
-                Target
-                Quantity_mean
-                Sample
-                Task
-            if replace_bloq is true
-                bloq
-                lowest_std_quantity
-      Returns
-      qpcr_m: same data, with additional columns
-            mean_normalized_to_pmmov: takes every column and divides by PMMoV that is associated with that sample name (so where target == PMMoV it will be 1)
-            log10mean_normalized_to_log10pmmov: takes the log10 of N1 and the log 10 of PMMoV then normalizes
-            log10_mean_normalized_to_pmmov: takes the log10 of mean_normalized_to_pmmov
-    '''
-    if replace_bloq:
-        qpcr_data.loc[qpcr_data.bloq==True, "Quantity_mean"]= qpcr_data.lowest_std_quantity
-
-    pmmov=qpcr_data[qpcr_data.Target=='PMMoV']
-    pmmov=pmmov[['Quantity_mean','Sample']]
-    pmmov.columns=['pmmov_mean',  "Sample"]
-    qpcr_m=qpcr_data.merge(pmmov, how='left', on=["Sample"])
-    qpcr_m["mean_normalized_to_pmmov"] = qpcr_m['Quantity_mean']/qpcr_m['pmmov_mean']
-    qpcr_m["log10mean_normalized_to_log10pmmov"] = np.log10(qpcr_m['Quantity_mean'])/np.log10(qpcr_m['pmmov_mean'])
-    qpcr_m['log10_mean_normalized_to_pmmov']=np.log10(qpcr_m['mean_normalized_to_pmmov'])
-
-    return qpcr_m
-
-
 def calculate_recovery(sample_data_qpcr, spike_name):
     '''
     Given the sample_data_qpcr dataframe,
